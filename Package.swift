26159c1c676d714631892aa0f32e1aa17397e98b
Full Queue Developer <103295874+FullQueueDeveloper@users.noreply.github.com>
2023-08-19 14:29:58 -0400

    Upgrade to Sh 1.3 (#1)
    * Upgrade to Sh 1.3
    
    * github actions
    
    ---------
    
    Co-authored-by: Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
   name: "ShGit",
   platforms: [
-    .macOS(.v12),
+    .macOS(.v13),
   ],
   products: [
     .library(name: "ShGit", targets: ["ShGit"])
   ],
   dependencies: [
-    .package(url: "https://github.com/FullQueueDeveloper/Sh.git", from: "1.2.0"),
+    .package(url: "https://github.com/FullQueueDeveloper/Sh.git", from: "1.3.0"),
   ],
   targets: [
     .target(name: "ShGit", dependencies: [
